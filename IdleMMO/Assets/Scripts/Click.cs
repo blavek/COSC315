1cb618014541b74a9095ef19810ade64333ceaac
blavek <blavek@comcast.net>
2015-11-05 22:29:08 -0500

    click fix

--- IdleMMO/Assets/Scripts/Click.cs
+++ IdleMMO/Assets/Scripts/Click.cs
@@ -4,25 +4,17 @@ using System.Collections;
 public class Click {
     public int damage = 1;
 
-	public float playerDamage = 1; 
-	public float monsterHealth= 10; 
+    public float playerDamage = 1;
+    public float monsterHealth = 10;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
 
 
-	
-	}
-	
-	// Update is called once per frame
-	void Update (){
 
-		public class ClickDamage{
+    }
 
-			if (Input.GetMouseButtonDown(0)){
-				monsterHealth = monsterHealth - playerDamage;
-				Debug.Log ("You have clicked the monster ");
-
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update() {
+    }
+}
