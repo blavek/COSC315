466081deea518508070ab6fad784ec939a365692
Jared Rixter <jaredrixter@jareds-MacBook-Pro.local>
2015-11-05 23:08:09 -0500

    Click Script again
    Hopefully this one works

--- IdleMMO/Assets/Scripts/Click.cs
+++ IdleMMO/Assets/Scripts/Click.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections; 
 
-public class Click {
-    public int damage = 1;
-
-    public float playerDamage = 1;
-    public float monsterHealth = 10;
-
-    // Use this for initialization
-    void Start() {
-
-
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-    }
+public class Click{
+	
+public float monsterHealth = 10f;
+public float playerDamage = 1f; 
+public bool upgraded = false;
+	
+	void clickDamage () {
+		if (Input.GetMouseButtonDown(0))
+			monsterHealth = monsterHealth - playerDamage; 
+		debug.Log ( "You have clicked the monster ");
+		
+		upgradedDamage () {
+			if (upgraded = true);
+			playerDamage = playerDamage * 2;
+		}
+		
+	}
+	
 }
