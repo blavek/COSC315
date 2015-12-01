64e177eeba1f0f07bf27362afda9ce61ae0ba8ab
blavek <blavek@comcast.net>
2015-12-01 17:47:39 -0500

    quick in class
    stuff

--- IdleMMO/Assets/Scripts/enemyHPTextDisplay.cs
+++ IdleMMO/Assets/Scripts/enemyHPTextDisplay.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using UnityEngine.UI;
 
 
-public class enemyHPTextDisplay : Enemy{
+public class enemyHPTextDisplay : Enemy {
 
 	public Text enemyHPText;
 	private RectTransform enemyHealth;
+	
 	// Use this for initialization
 	void Start () {
 		enemyHPText = GetComponent<Text>();
@@ -14,7 +15,9 @@ public class enemyHPTextDisplay : Enemy{
 	
 	// Update is called once per frame
 	void Update () {
-		enemyHPText.text = getHealth().ToString();
-		enemyHPText.GetComponent<Text>().text = "Enemy Health \n" + enemyHealth;
+		int a;
+		a = (int)getHealth ();
+		enemyHPText.text = a.ToString();
+//		enemyHP.GetComponent<Text>().text = "Enemy Health \n" + player.getPlayerDamage() + " Per Click";
 	}
 }
